be9026e0f48eac1f5716764e1d5f7b3b799f3735
Martin Torres <103221160+MartinT2222@users.noreply.github.com>
2023-10-06 10:43:27 -0300

    Configuramos aplicacacion Usurario
    agregamos forms, urls y los views para la configuración del registro de usuarios

--- ecommerceAlimentosNaturales/USUARIOS/views.py
+++ ecommerceAlimentosNaturales/USUARIOS/views.py
@@ -1,3 +1,21 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .forms import *
 
-# Create your views here.
+
+
+def registrarse(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()            
+            return HttpResponseRedirect(reverse('login'))
+        else:            
+            return HttpResponseRedirect(reverse('registrarse'))
+
+    else:        
+        form = RegistroForm()
+        return render(request, 'registration/registro.html', {
+        'form': form
+        })
